0c9e4e7a453939043b2e91c4a1b927641166a027
Joe Russo <thejoerusso@gmail.com>
2013-05-08 14:38:36 -0400

    Adding buttons

--- ST Remote Test/DangerTestFirstViewController.m
+++ ST Remote Test/DangerTestFirstViewController.m
@@ -20,6 +20,23 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+- (IBAction)inputSelect:(id)sender{
+    UIButton *button = (UIButton *)sender;
+    NSString *title = button.titleLabel.text.uppercaseString;//how to write this without dot notation?
+    
+    //is this a sloppy way of doing XOR buttons?
+    [an1Button setSelected:NO];
+    [an2Button setSelected:NO];
+    [an3Button setSelected:NO];
+    [an4Button setSelected:NO];
+    
+    [sender setSelected:YES];
+    
+    NSLog(@"SET %@", title);
+
+}
+
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
